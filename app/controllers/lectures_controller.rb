610925f3b6dfd5f76ea8e75020f8939e2212d644
thebrianemory <the@brianemory.com>
2016-08-04 23:24:33 -0700

    Add lecture create action and youtube importer

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,2 +1,13 @@
 class LecturesController < ApplicationController
+
+  def create
+    @lecture = Lecture.import_youtube(lecture_params)
+    binding.pry
+    @lecture.save
+  end
+
+  private
+  def lecture_params
+    params.require(:lecture).permit(:url, :topic)
+  end
 end
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -2,17 +2,18 @@ class Lecture < ActiveRecord::Base
   validates_presence_of :title, :topic, :duration, :url, :published, :embed
   validates_uniqueness_of :url
 
-  def self.import_youtube(url)
+  def self.import_youtube(url, topic)
 
     binding.pry
     video = Yt::Video.new url: url
     title = video.title
+    topic = topic
+    url = url
     duration = video.duration
     published = video.published_at
-    url = url
     embed = video.embed_html
 
-    # lecture.new(title: video.title, topic: 'TBD', url: url, )
+    lecture.new(title: title, topic: topic, url: url, duration: duration, published: published, embed: embed)
   end
 
   # https://www.youtube.com/watch?v=t2A6xPbh0I8
