0d233a9a169bccc88530fa122c22670eda14f03c
thebrianemory <the@brianemory.com>
2016-08-04 23:27:03 -0700

    Make index and show render as json

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,5 +1,13 @@
 class LecturesController < ApplicationController
 
+  def index
+    render json: Lecture.all
+  end
+
+  def show
+    render json: Lecture.find(params[:id])
+  end
+
   def create
     @lecture = Lecture.import_youtube(lecture_params)
     binding.pry
