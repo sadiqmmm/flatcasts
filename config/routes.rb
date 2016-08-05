4b3a1b3b344d711fe31977556c6fe585155cc691
thebrianemory <the@brianemory.com>
2016-08-04 23:25:51 -0700

    Update routes for getting lectures

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :lectures
+  scope :api do
+    get "/lectures(.:format)" => "lectures#index"
+    get "/lectures/:id(.:format)" => "lectures#show"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
