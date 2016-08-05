4d59fd5006e9d1cee8b81ed4b6fe4210ba10d39b
thebrianemory <the@brianemory.com>
2016-08-04 22:05:02 -0700

    Run db:create and reset

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160805044949) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lectures", force: :cascade do |t|
     t.string   "title"
     t.string   "topic"
