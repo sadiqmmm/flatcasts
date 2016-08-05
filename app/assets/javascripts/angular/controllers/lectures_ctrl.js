23b5d9c0beb87397559e071efc0f1e58ee5602e3
thebrianemory <the@brianemory.com>
2016-08-05 10:08:57 -0700

    Add video embed scope to lecture ctrl

--- app/assets/javascripts/angular/controllers/lectures_ctrl.js
+++ app/assets/javascripts/angular/controllers/lectures_ctrl.js
@@ -1,9 +1,9 @@
 App.controller('LecturesCtrl', [
   '$scope', 'Lecture', function($scope, Lecture) {
     $scope.lectures = Lecture.query();
-    $scope.theBestVideo = 'sMKoNBRZM1M';
     $scope.selectedLecture = null;
     return $scope.showLecture = function(lecture) {
+      $scope.lectureEmbed = lecture.video_id;
       return $scope.selectedLecture = lecture;
     };
   }
