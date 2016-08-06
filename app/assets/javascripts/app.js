30932a32513aeefe0e570578e3d5f5bf0f65e376
thebrianemory <the@brianemory.com>
2016-08-05 21:25:07 -0700

    Add remaining view page skeletons

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,32 @@
 angular.module('Flatcasts', ['ngResource', 'youtube-embed', 'ui.router', 'templates'])
-  .config(function ($stateProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
     .state('home', {
       url: '/',
       templateUrl: 'index.html',
       controller: 'LecturesCtrl'
     })
+    .state('lecture', {
+      url: '/lectures',
+      templateUrl: 'lectures/index.html'
+    })
+    .state('create', {
+      url: '/lectures/create',
+      templateUrl: 'lectures/create.html'
+    })
+    .state('search', {
+      url: '/lectures/search',
+      templateUrl: 'lectures/search.html'
+    })
+    .state('show', {
+      url: '/lectures/:id',
+      controller: 'LecturesCtrl as lecture',
+      templateUrl: 'lectures/show.html'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'about.html'
+    })
+
+    $urlRouterProvider.otherwise('/');
   })
