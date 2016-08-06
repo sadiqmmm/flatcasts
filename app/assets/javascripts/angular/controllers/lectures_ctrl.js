0fc002158c8e9e25baba177863ca73b199bb853f
thebrianemory <the@brianemory.com>
2016-08-06 10:08:24 -0700

    Add filtering by topics for lectures

--- app/assets/javascripts/angular/controllers/lectures_ctrl.js
+++ app/assets/javascripts/angular/controllers/lectures_ctrl.js
@@ -1,6 +1,33 @@
 function LecturesCtrl($scope, Lecture) {
-    $scope.lectures = Lecture.query();
-  };
+  // Variables - Private
+  var self = this;
+
+  // Variables - Public
+  self.filter = {};
+
+  self.lectures = Lecture.query();
+
+  // Functions - Public
+  self.filterByTopic = filterByTopic;
+  self.getTopics = getTopics;
+
+  // Functions - Definitions
+  function filterByTopic(lecture) {
+    return self.filter[lecture.topic] || noFilter(self.filter);
+  }
+
+  function getTopics() {
+    return (self.lectures || []).
+      map(function (lecture) { return lecture.topic; }).
+      filter(function (cat, idx, arr) { return arr.indexOf(cat) === idx; });
+  }
+
+  function noFilter(filterObj) {
+    return Object.
+      keys(filterObj).
+      every(function (key) { return !filterObj[key]; });
+  }
+};
 
 LecturesCtrl.$inject = ['$scope', 'Lecture'];
 
