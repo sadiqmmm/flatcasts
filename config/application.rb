c5068557cb03d19707211f9023bae5063c632133
thebrianemory <the@brianemory.com>
2016-08-05 20:27:35 -0700

    Remove changes to appplication.rb

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,5 @@ module Flatcasts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.paths << Rails.root.join("vendor","assets")
-    Rails.application.config.assets.precompile += %w( *.css *.js )
   end
 end
