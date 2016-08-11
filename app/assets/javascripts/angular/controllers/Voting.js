53da64c9ccaa7ab005bcc0f518533c68f7d64eee
thebrianemory <the@brianemory.com>
2016-08-11 11:00:49 -0700

    Updating voting.js to update vote count

--- app/assets/javascripts/angular/controllers/Voting.js
+++ app/assets/javascripts/angular/controllers/Voting.js
@@ -1,13 +1,25 @@
-function Voting($scope, $http, Lecture, $stateParams) {
+function Voting($scope, $http, Lecture, $stateParams, GetLectureService) {
     $scope.lecture = {};
     var self = this;
     var lectureId = $stateParams.id;
+
+    this.lecture = GetLectureService
+      .getLecture(lectureId)
+      .then(function (res) {
+        $scope.lecture = res.data;
+      })
 
     $scope.upVote = function() {
     $http({
       method  : 'PUT',
       url     : '/api/lectures/' + lectureId + '/up_vote',
      })
+     this.lecture = GetLectureService
+       .getLecture(lectureId)
+       .then(function (res) {
+         $scope.lecture = res.data;
+         $scope.lectureEmbed = $scope.lecture.video_id;
+       })
     };
 
     $scope.downVote = function() {
@@ -15,9 +27,15 @@ function Voting($scope, $http, Lecture, $stateParams) {
       method  : 'PUT',
       url     : '/api/lectures/' + lectureId + '/down_vote',
      })
+     this.lecture = GetLectureService
+       .getLecture(lectureId)
+       .then(function (res) {
+         $scope.lecture = res.data;
+         $scope.lectureEmbed = $scope.lecture.video_id;
+       })
     };
 };
 
-Voting.$inject = ['$scope', '$http', 'Lecture', '$stateParams'];
+Voting.$inject = ['$scope', '$http', 'Lecture', '$stateParams', 'GetLectureService'];
 
 angular.module('Flatcasts').controller('Voting', Voting);
