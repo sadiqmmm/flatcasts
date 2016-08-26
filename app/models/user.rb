292d1693a9d53c8f6ee1113109e44166ea5d2f63
thebrianemory <the@brianemory.com>
2016-08-26 09:01:12 -0700

    Remake user model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,2 @@
 class User < ActiveRecord::Base
-  validates_presence_of :github_uid, :name, :github_token
-  validates_uniqueness_of :github_uid, :display_name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826153655) do
+ActiveRecord::Schema.define(version: 20160826160043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,13 +30,11 @@ ActiveRecord::Schema.define(version: 20160826153655) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "github_uid",   null: false
-    t.string   "name",         null: false
-    t.string   "display_name"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-
-  add_index "users", ["github_uid"], name: "index_users_on_github_uid", unique: true, using: :btree
 
 end
