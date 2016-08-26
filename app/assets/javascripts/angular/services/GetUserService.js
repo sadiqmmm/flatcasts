7d1f11af2bca3dbfd94ebb1bf4747583d42d952c
thebrianemory <the@brianemory.com>
2016-08-26 09:59:50 -0700

    Remove all user stuff for angular (starting over)

--- app/assets/javascripts/angular/services/GetUserService.js
+++
@@ -1,9 +0,0 @@
-function GetUserService($http) {
-  this.getUser = function (id) {
-    return $http.get('/api/users/' + id)
-  }
-};
-
-GetUserService.$inject = ['$http'];
-
-angular.module('Flatcasts').service('GetUserService', GetUserService);
