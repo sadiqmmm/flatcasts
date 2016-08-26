42d646560c5fdc20b8c3ba5cc0d9dc6c19753db6
thebrianemory <the@brianemory.com>
2016-08-26 08:38:56 -0700

    Add user model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805044949) do
+ActiveRecord::Schema.define(version: 20160826153655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,20 @@ ActiveRecord::Schema.define(version: 20160805044949) do
     t.string   "published"
     t.string   "embed"
     t.string   "video_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "votes",      default: 0
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "github_uid",   null: false
+    t.string   "name",         null: false
+    t.string   "display_name"
+    t.string   "email"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "users", ["github_uid"], name: "index_users_on_github_uid", unique: true, using: :btree
 
 end
