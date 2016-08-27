a16fb85a2e14f4b3f141855b7fab74c5398bac40
thebrianemory <the@brianemory.com>
2016-08-27 15:22:25 -0700

    Add conditional for current_user variable

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,29 +2,42 @@ angular.module('Flatcasts', ['ngResource', 'youtube-embed', 'ui.router', 'templa
   .config(function ($stateProvider, $urlRouterProvider, paginationTemplateProvider) {
     paginationTemplateProvider.setPath('pagination/mine.tpl.html');
 
-    $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'index.html',
-      controller: 'LecturesCtrl'
-    })
-    .state('lecture', {
-      url: '/lectures',
-      templateUrl: 'lectures/index.html'
-    })
-    .state('create', {
-      url: '/lectures/create',
-      templateUrl: 'lectures/create.html'
-    })
-    .state('show', {
-      url: '/lectures/:id',
-      controller: 'LectureCtrl as lecture',
-      templateUrl: 'lectures/show.html'
-    })
-    .state('about', {
-      url: '/about',
-      templateUrl: 'about.html'
-    })
+    if (current_user === null) {
+      $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'index2.html',
+        controller: 'LecturesCtrl'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'about.html'
+      })
+    } else {
+      $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'index.html',
+        controller: 'LecturesCtrl'
+      })
+      .state('lecture', {
+        url: '/lectures',
+        templateUrl: 'lectures/index.html'
+      })
+      .state('create', {
+        url: '/lectures/create',
+        templateUrl: 'lectures/create.html'
+      })
+      .state('show', {
+        url: '/lectures/:id',
+        controller: 'LectureCtrl as lecture',
+        templateUrl: 'lectures/show.html'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'about.html'
+      })
+    }
 
     $urlRouterProvider.otherwise('/');
   })
