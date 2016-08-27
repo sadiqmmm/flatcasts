fb72316ad71f000e3136eda33d60500f796bb04e
thebrianemory <the@brianemory.com>
2016-08-26 21:40:31 -0700

    Add angular user controller

---
+++ app/assets/javascripts/angular/controllers/UserCtrl.js
@@ -0,0 +1,15 @@
+function UserCtrl($scope, $http, $stateParams, GetUserService) {
+  var userId = $stateParams.id;
+
+  this.user = GetUserService.getUser(userId);
+  GetUserService
+    .getUser(userId)
+    .then(function (res) {
+      $scope.user = res.data;
+      $scope.userEmbed = $scope.user.video_id;
+    })
+};
+
+UserCtrl.$inject = ['$scope', '$http', '$stateParams', 'GetUserService'];
+
+angular.module('Flatcasts').controller('UserCtrl', UserCtrl);
