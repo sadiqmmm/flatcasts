70bbe725a1fc1806a7da9d59c7318b41a974d1e7
thebrianemory <the@brianemory.com>
2016-08-28 18:45:36 -0700

    Set up addFavorite in favorite service

--- app/assets/javascripts/angular/services/AddFavoriteService.js
+++ app/assets/javascripts/angular/services/AddFavoriteService.js
@@ -1,25 +1,9 @@
 function AddFavoriteService($http, GetLectureService) {
 
-    this.voteCount = function(lectureId, $scope) {
-      this.lecture = GetLectureService
-        .getLecture(lectureId)
-        .then(function (res) {
-          $scope.lecture = res.data;
-        })
-    }
-
-    this.upVote = function(lectureId, $scope) {
+    this.addFavorite = function(lectureId, $scope) {
     $http({
       method  : 'PUT',
       url     : '/api/lectures/' + lectureId + '/up_vote',
-     })
-     this.voteCount(lectureId, $scope);
-    };
-
-    this.downVote = function(lectureId, $scope) {
-    $http({
-      method  : 'PUT',
-      url     : '/api/lectures/' + lectureId + '/down_vote',
      })
      this.voteCount(lectureId, $scope);
     };
